10ad455535c671400f149e715b10ab9bb714f962
viktorasbuivydas <viktorasss00@gmail.com>
2025-04-21 10:19:55 +0300

    logger update

--- src/logger.ts
+++ src/logger.ts
@@ -3,6 +3,10 @@ import winston from "winston";
 export const logger = winston.createLogger({
   level: "debug",
   format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "logfile.log" }),
+  ],
 });
 
 //
